1b4c248418be842c5328fafa92fe09b8158a8be4
Aerendir <hello@aerendir.me>
2020-07-22 11:38:58 +0200

    Update config of PHP CS Fixer.

--- src/Property/HasPlainPasswordTrait.php
+++ src/Property/HasPlainPasswordTrait.php
@@ -3,12 +3,12 @@
 declare(strict_types=1);
 
 /*
- * This file is part of SHQUsersBundle.
+ * This file is part of the Serendipity HQ Users Bundle.
  *
- * (c) Adamo Aerendir Crespi <aerendir@serendipityhq.com>.
+ * Copyright (c) Adamo Aerendir Crespi <aerendir@serendipityhq.com>.
  *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace SerendipityHQ\Bundle\UsersBundle\Property;
