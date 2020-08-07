e9eea58074fd341da59c685a2d70b8e9cfeede05
Aerendir <hello@aerendir.me>
2020-08-07 11:25:23 +0200

    Update baselines.

--- .baseline-phan.php
+++ .baseline-phan.php
@@ -10,29 +10,22 @@
 return [
     // # Issue statistics:
     // PhanRedefinedClassReference : 15+ occurrences
-    // PhanUndeclaredTypeThrowsType : 10+ occurrences
-    // UndeclaredTypeInInlineVar : 7 occurrences
-    // PhanUndeclaredClassMethod : 6 occurrences
-    // PhanUndeclaredTypeParameter : 6 occurrences
     // PhanUnreferencedPublicMethod : 6 occurrences
-    // PhanUndeclaredMethod : 4 occurrences
-    // PhanUndeclaredTypeProperty : 4 occurrences
-    // PhanUndeclaredTypeReturnType : 4 occurrences
+    // PhanUndeclaredMethod : 5 occurrences
     // PhanUnreferencedClass : 3 occurrences
+    // PhanRedefinedExtendedClass : 2 occurrences
+    // PhanTypeMismatchArgumentNullable : 2 occurrences
     // PhanCommentParamWithoutRealParam : 1 occurrence
-    // PhanRedefinedExtendedClass : 1 occurrence
+    // PhanTypeMismatchArgument : 1 occurrence
     // PhanTypeMismatchDeclaredParam : 1 occurrence
-    // PhanTypeNoPropertiesForeach : 1 occurrence
     // PhanUnreferencedProtectedProperty : 1 occurrence
 
     // Currently, file_suppressions and directory_suppressions are the only supported suppressions
     'file_suppressions' => [
-        'src/Command/UsersCreateCommand.php' => ['PhanRedefinedClassReference', 'PhanRedefinedExtendedClass', 'PhanTypeNoPropertiesForeach', 'PhanUndeclaredClassMethod', 'PhanUndeclaredTypeParameter', 'PhanUndeclaredTypeProperty', 'PhanUndeclaredTypeReturnType', 'PhanUndeclaredTypeThrowsType', 'PhanUnreferencedProtectedProperty', 'UndeclaredTypeInInlineVar'],
+        'src/Command/UsersCreateCommand.php' => ['PhanRedefinedClassReference', 'PhanRedefinedExtendedClass', 'PhanUndeclaredMethod', 'PhanUnreferencedProtectedProperty'],
         'src/DependencyInjection/SHQUsersExtension.php' => ['PhanUnreferencedClass'],
-        'src/Doctrine/UserEncodePasswordListener.php' => ['PhanCommentParamWithoutRealParam', 'PhanUndeclaredClassMethod', 'PhanUndeclaredMethod', 'PhanUndeclaredTypeParameter', 'PhanUndeclaredTypeProperty', 'PhanUndeclaredTypeThrowsType', 'PhanUnreferencedPublicMethod', 'UndeclaredTypeInInlineVar'],
-        'src/Event/UserCreatedEvent.php' => ['PhanTypeMismatchDeclaredParam', 'PhanUndeclaredTypeParameter', 'PhanUndeclaredTypeProperty', 'PhanUndeclaredTypeReturnType', 'PhanUnreferencedPublicMethod', 'UndeclaredTypeInInlineVar'],
-        'src/Manager/UsersManager.php' => ['PhanUndeclaredClassMethod', 'PhanUndeclaredTypeParameter', 'PhanUndeclaredTypeProperty', 'PhanUndeclaredTypeReturnType', 'UndeclaredTypeInInlineVar'],
-        'src/Manager/UsersManagerInterface.php' => ['PhanUndeclaredTypeParameter', 'PhanUndeclaredTypeReturnType', 'PhanUndeclaredTypeThrowsType'],
+        'src/Doctrine/UserEncodePasswordListener.php' => ['PhanCommentParamWithoutRealParam', 'PhanTypeMismatchArgument', 'PhanTypeMismatchArgumentNullable', 'PhanUndeclaredMethod', 'PhanUnreferencedPublicMethod'],
+        'src/Event/UserCreatedEvent.php' => ['PhanRedefinedExtendedClass', 'PhanTypeMismatchDeclaredParam', 'PhanUnreferencedPublicMethod'],
         'src/Manager/UsersManagerRegistry.php' => ['PhanUnreferencedPublicMethod'],
         'src/Property/HasPlainPasswordTrait.php' => ['PhanUnreferencedClass', 'PhanUnreferencedPublicMethod'],
         'src/SHQUsersBundle.php' => ['PhanUnreferencedClass'],
