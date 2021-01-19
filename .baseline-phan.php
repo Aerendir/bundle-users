8e282443d032b0b06c7a0eb93663ce71488c07ff
Aerendir <hello@aerendir.me>
2021-01-19 13:40:49 +0100

    Update baselines.

--- .baseline-phan.php
+++ .baseline-phan.php
@@ -9,43 +9,35 @@
  */
 return [
     // # Issue statistics:
-    // PhanUnreferencedPublicMethod : 25+ occurrences
-    // PhanRedefinedClassReference : 15+ occurrences
+    // PhanRedefinedClassReference : 30+ occurrences
     // PhanAccessMethodInternal : 9 occurrences
-    // PhanUndeclaredMethod : 5 occurrences
     // PhanUnreferencedClass : 3 occurrences
-    // PhanUnusedPublicFinalMethodParameter : 3 occurrences
     // PhanAccessClassConstantInternal : 2 occurrences
-    // PhanTypeMismatchArgumentNullable : 2 occurrences
-    // PhanWriteOnlyPrivateProperty : 2 occurrences
-    // PhanReadOnlyPublicProperty : 1 occurrence
     // PhanRedefinedExtendedClass : 1 occurrence
-    // PhanTypeMismatchArgument : 1 occurrence
-    // PhanTypeMismatchDeclaredParam : 1 occurrence
-    // PhanUndeclaredClassReference : 1 occurrence
-    // PhanUnreferencedProtectedProperty : 1 occurrence
-    // UndeclaredTypeInInlineVar : 1 occurrence
+    // PhanUndeclaredMethod : 1 occurrence
+    // PhanUnusedPublicFinalMethodParameter : 1 occurrence
+    // PhanWriteOnlyPrivateProperty : 1 occurrence
 
     // Currently, file_suppressions and directory_suppressions are the only supported suppressions
     'file_suppressions' => [
-        'src/Command/UserCreateCommand.php' => ['PhanRedefinedClassReference', 'PhanRedefinedExtendedClass', 'PhanUndeclaredMethod', 'PhanUnreferencedProtectedProperty', 'UndeclaredTypeInInlineVar'],
-        'src/DependencyInjection/SHQUsersExtension.php' => ['PhanUnreferencedClass'],
-        'src/Doctrine/UserEncodePasswordListener.php' => ['PhanTypeMismatchArgument', 'PhanTypeMismatchArgumentNullable', 'PhanUndeclaredMethod', 'PhanUnreferencedPublicMethod'],
-        'src/Event/PasswordResetTokenCreatedEvent.php' => ['PhanUnreferencedPublicMethod'],
-        'src/Event/PasswordResetTokenCreationFailedEvent.php' => ['PhanUnreferencedPublicMethod'],
-        'src/Event/UserCreatedEvent.php' => ['PhanTypeMismatchDeclaredParam', 'PhanUnreferencedPublicMethod'],
-        'src/Form/Type/ConfirmedPasswordType.php' => ['PhanUnusedPublicFinalMethodParameter'],
-        'src/Form/Type/PasswordResetRequestType.php' => ['PhanUnusedPublicFinalMethodParameter'],
+        'src/Command/AbstractUserActivationCommand.php' => ['PhanRedefinedClassReference'],
+        'src/Command/AbstractUserRolesCommand.php' => ['PhanRedefinedClassReference'],
+        'src/Command/AbstractUsersCommand.php' => ['PhanRedefinedClassReference'],
+        'src/Command/RoleAddCommand.php' => ['PhanRedefinedClassReference'],
+        'src/Command/RoleRemCommand.php' => ['PhanRedefinedClassReference'],
+        'src/Command/UserActivateCommand.php' => ['PhanRedefinedClassReference'],
+        'src/Command/UserCreateCommand.php' => ['PhanRedefinedClassReference', 'PhanUndeclaredMethod'],
+        'src/Command/UserDeactivateCommand.php' => ['PhanRedefinedClassReference'],
+        'src/DependencyInjection/SHQUsersExtension.php' => ['PhanRedefinedClassReference', 'PhanUnreferencedClass'],
         'src/Form/Type/UserPasswordChangeType.php' => ['PhanUnusedPublicFinalMethodParameter'],
-        'src/Helper/PasswordHelper.php' => ['PhanUnreferencedPublicMethod'],
-        'src/Helper/PasswordResetHelper.php' => ['PhanAccessMethodInternal', 'PhanUnreferencedPublicMethod', 'PhanWriteOnlyPrivateProperty'],
-        'src/Manager/Exception/UsersManagerException.php' => ['PhanUndeclaredClassReference'],
-        'src/Manager/PasswordManager.php' => ['PhanAccessMethodInternal', 'PhanReadOnlyPublicProperty', 'PhanUnreferencedPublicMethod'],
-        'src/Manager/UsersManagerRegistry.php' => ['PhanUnreferencedPublicMethod'],
-        'src/Model/PasswordResetTokenPublic.php' => ['PhanUnreferencedPublicMethod'],
-        'src/Model/Property/HasPlainPasswordTrait.php' => ['PhanUnreferencedClass', 'PhanUnreferencedPublicMethod'],
-        'src/Model/Property/PasswordResetTokenTrait.php' => ['PhanUnreferencedClass', 'PhanUnreferencedPublicMethod', 'PhanWriteOnlyPrivateProperty'],
-        'src/Util/PasswordResetTokenGenerator.php' => ['PhanAccessClassConstantInternal', 'PhanAccessMethodInternal', 'PhanRedefinedClassReference'],
+        'src/Helper/PasswordResetHelper.php' => ['PhanAccessMethodInternal'],
+        'src/Manager/PasswordManager.php' => ['PhanAccessMethodInternal', 'PhanRedefinedClassReference'],
+        'src/Manager/UsersManager.php' => ['PhanRedefinedClassReference'],
+        'src/Manager/UsersManagerInterface.php' => ['PhanRedefinedClassReference'],
+        'src/Model/Property/HasPlainPasswordTrait.php' => ['PhanUnreferencedClass'],
+        'src/Model/Property/PasswordResetTokenTrait.php' => ['PhanUnreferencedClass', 'PhanWriteOnlyPrivateProperty'],
+        'src/Repository/PasswordResetTokenRepository.php' => ['PhanRedefinedClassReference', 'PhanRedefinedExtendedClass'],
+        'src/Util/PasswordResetTokenGenerator.php' => ['PhanAccessClassConstantInternal', 'PhanAccessMethodInternal'],
     ],
     // 'directory_suppressions' => ['src/directory_name' => ['PhanIssueName1', 'PhanIssueName2']] can be manually added if needed.
     // (directory_suppressions will currently be ignored by subsequent calls to --save-baseline, but may be preserved in future Phan releases)
